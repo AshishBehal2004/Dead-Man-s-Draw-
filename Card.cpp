ec09a09fa847af900b8ead381196ba701f88db27
Ashish Behal <behay002@mymail.unisa.edu.au>
2026-03-24 19:41:40 +1030

    Working on Setting up the Card Class

--- Card.cpp
+++ Card.cpp
@@ -1,1 +1,24 @@
 #include "Card.h"
+#include <string>
+#include "Player.h"
+#include "Game.h"
+
+
+
+typedef std::vector<Card*> CardCollection;
+
+enum CardType {
+    Cannon, Chest, Key, Anchor, Sword, Hook, Oracle, Map, Mermaid, Kraken
+};
+
+std::string str() {
+
+}
+
+void play(Game& game, Player& player) {
+
+}
+
+void willAddToBank(Game& game, Player& player) {
+
+}
--- Card.h
+++ Card.h
@@ -1,5 +1,28 @@
 #pragma once
+#include <string>
+
+#include "Player.h"
+#include "Game.h"
+
+#include <vector>
+
 class Card
 {
+public:
+    enum CardType {
+        Cannon, Chest, Key, Anchor, Sword, Hook, Oracle, Map, Mermaid, Kraken
+    };
+    const CardType& type() const;
+
+
+    virtual std::string str() const = 0;
+
+    virtual void play(Game &game, Player &player) = 0;
+
+    virtual void willAddToBank(Game& game, Player& player) {}
+
+    typedef std::vector<Card*> CardCollection;
+
+
 };
 
