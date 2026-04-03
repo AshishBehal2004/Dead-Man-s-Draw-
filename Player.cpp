75ae90936ae3ca80d1678685292a11362d395612
Ashish Behal <behay002@mymail.unisa.edu.au>
2026-04-03 14:22:48 +1030

    Add Player Constructor, getter for private attributes

--- Player.cpp
+++ Player.cpp
@@ -1,6 +1,52 @@
 #include "Player.h"
 #include "Card.h"
 #include "Game.h"
-bool playCard(Card* card, Game& game) {
+
+
+Player::Player() : _current_total_score(0), _player_name("") {
+    std::string names[] = { "Sam", "Billy", "Jen", "Bob", "Sally", "Joe", "Sue",
+"Sasha", "Tina", "Marge" };
+    _player_name = names[rand() % 10];
+}
+
+int Player::get_current_total_score() const {
+    return _current_total_score;
+}
+
+std::string Player::get_player_name() const {
+    return _player_name;
+}
+
+const Card::CardCollection& Player::get_bank() const {  // reference
+    return _bank;
+}
+
+const Card::CardCollection& Player::get_play_Area() const{  //reference
+    return _playArea;
+}
+
+bool Player::playCard(Card* card, Game& game) {
     return false;
+}
+
+void Player::manage_cards() {
+
+}
+
+void Player::add_card() {
+
+}
+
+// Is responsible for knowing if the player’s current play area is bust(i.e.there are two
+//   of the same type of card in the play area).
+bool Player::is_bust() {
+
+}
+
+std::string Player::print_playArea_Bank() {
+
+}
+
+void Player::move_cards() {
+
 }
--- Player.h
+++ Player.h
@@ -1,13 +1,35 @@
 #pragma once
-class Card;
+#include "Card.h"
 class Game;
 
 class Player
 {
 
+private:
+    int _current_total_score;
+    std::string _player_name;
+    Card::CardCollection _bank;
+    Card::CardCollection _playArea;
 
 public:
+    
+    Player();
+
+    int get_current_total_score() const;
+    std::string get_player_name() const;
+    const Card::CardCollection& get_bank() const;  // reference
+    const Card::CardCollection& get_play_Area() const;  //reference
+
+    void manage_cards();
+    void add_card();
+    void move_cards();
+
     bool playCard(Card* card, Game& game);
+    bool is_bust();
+    std::string print_playArea_Bank();
+
+    
 
 };
+
 
